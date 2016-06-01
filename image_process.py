5f90690404cebd7f3f558bc03693bc7bfd703501
Eric <webere@lafayette.edu>
2016-06-01 13:12:07 -0700

    made input and output directories command line arguments

--- image_process.py
+++ image_process.py
@@ -4,39 +4,37 @@ import os
 import PIL
 from PIL import Image
 
-totalWidth = int(sys.argv[1])
-imagesPerRow = int(sys.argv[2])
+pathToImages = sys.argv[1]
+pathToThumbs = sys.argv[2]
+totalWidth = int(sys.argv[3])
+imagesPerRow = int(sys.argv[4])
 
 print("Width: " + str(totalWidth))
 print("Images Per Row: " + str(imagesPerRow))
 
-#for filename in os.listdir("./test images"):
-	#print(filename)
+filenames = os.listdir(pathToImages)
 
 index = 0
-while (index + imagesPerRow <= len(os.listdir("./test images"))):
+while (index + imagesPerRow <= len(filenames)):
 	minHeight = None
 	for i in range(index, index + imagesPerRow):
-		if (minHeight == None or Image.open("test images/" + os.listdir("./test images")[i]).size[1] < minHeight):
-			minHeight = Image.open("test images/" + os.listdir("./test images")[i]).size[1] 
-		#print(str(i) + ": " + str(Image.open("test images/" + os.listdir("./test images")[i]).size[1]))
-	#print("MinHeight: " + str(minHeight))
-	
+		filename = filenames[i]
+		currentImage = Image.open(pathToImages + "/" + filename)
+		if (minHeight == None or currentImage.size[1] < minHeight):
+			minHeight = currentImage.size[1]
+
 	width = 0.0
 	for i in range(index, index + imagesPerRow):
-                width += Image.open("test images/" + os.listdir("./test images")[i]).size[0] * (float(minHeight)/Image.open("test images/" + os.listdir("./test images")[i]).size[1])
-		#print(os.listdir("./test images")[i] + " " + str(width))
-	#print("Width: " + str(width))
-	widthRatio = totalWidth/width
-	#print("Width Ratio: " + str(widthRatio))
+		filename = filenames[i]
+		currentImage = Image.open(pathToImages + "/" + filename)
+		width += currentImage.size[0] * (float(minHeight)/currentImage.size[1])
 
+	widthRatio = totalWidth/width
 	for i in range(index, index + imagesPerRow):
-		filename = os.listdir("./test images")[i]
+		filename = filenames[i]
 		print(filename)
-		currentImage = Image.open("test images/" + filename)
+		currentImage = Image.open(pathToImages + "/" + filename)
 		scaleRatio = widthRatio * minHeight/currentImage.size[1]
-		currentImage.resize((int(currentImage.size[0] * scaleRatio), int(currentImage.size[1] * scaleRatio))).save("thumbs/thumb" + filename) 
+		currentImage.resize((int(currentImage.size[0] * scaleRatio), int(currentImage.size[1] * scaleRatio))).save(pathToThumbs + "/" + filename) 
 	
 	index = index + imagesPerRow
-
-#Image.open("test images/" + filename).resize((totalWidth, totalHeight)).save("thumbs/thumb" + filename)
