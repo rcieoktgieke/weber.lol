11cb17ff93b73ce7dc369266ba953331e5e714f1
Eric <webere@lafayette.edu>
2016-06-01 16:52:51 -0700

    make output files PNGs and perform compression

--- image_process.py
+++ image_process.py
@@ -35,6 +35,6 @@ while (index + imagesPerRow <= len(filenames)):
 		print(filename)
 		currentImage = Image.open(pathToImages + "/" + filename)
 		scaleRatio = widthRatio * minHeight/currentImage.size[1]
-		currentImage.resize((int(currentImage.size[0] * scaleRatio), int(currentImage.size[1] * scaleRatio))).save(pathToThumbs + "/" + filename) 
+		currentImage.resize((int(currentImage.size[0] * scaleRatio), int(currentImage.size[1] * scaleRatio))).save(pathToThumbs + "/thumb" + str(i) + ".png", format="PNG", optimize=True)
 	
 	index = index + imagesPerRow
